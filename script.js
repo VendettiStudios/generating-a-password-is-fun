67ceb599095d9fdb0c8e027d234fe23f89b51c11
DH <daniel@vendettistudios.com>
2022-05-16 07:54:29 -0700

    defined variables for random password generation

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// dont change
+let generateBtn = document.querySelector('#generate');
+
+// defining function variables to generate a random password
+function generatePassword() {
+  let lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  let upperCase = 'ABCDEFGHIJKLMNOPRSTUVWXYZ';
+  let numbers = '1234567890';
+  let specialChar = '~!@#$%^&*()_+{}:?><;.,';
+  let password = '';
+  let avalChars = '';
+
+ 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
